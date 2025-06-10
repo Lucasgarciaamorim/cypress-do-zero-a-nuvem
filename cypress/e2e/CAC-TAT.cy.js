5373ea4348848aa0710fceae5a1477236adf5fea
lucas <lucasgarciaamorim@gmail.com>
2025-06-09 21:51:49 -0300

    Implement exercises 1–7

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -1,56 +1,102 @@
 describe('Central de Atendimento ao Cliente TAT', () => {
 
 
-    beforeEach(function () {
+    beforeEach( ()=> {
         cy.visit("./src/index.html")
     })
 
 
-    it('Verificar o título da aplicação', () => {
+    it('Check the aplication title', () => {
 
         cy.title().should('be.equal', 'Central de Atendimento ao Cliente TAT')
 
     });
 
-    it('Preencher os campos obrigatórios e envia o formulário ', () => {
+    //----------------------------Exercício 01----------------------------//
+    it('Fill in mandatory fields and send the form', () => {
+
+        const longText = Cypress._.repeat(' Thanks 1000x ', 5)
 
         cy.get('#firstName').type("Lucas ")
         cy.get('#lastName').type("Garcia Amorim")
         cy.get('#email').type('Lucasgarciamorim@gmail.com')
         cy.get('#phone').type('71999799831')
-        cy.get('#open-text-area').type('Testando o Cypress', {delay: 20})
+        cy.get('#open-text-area').type(longText, {delay: 5})
+        cy.get('button[type="submit"]').click()
 
-        cy.get('.button').click()
+
         cy.get('.success').should('be.visible',)
 
     })
-    it('Exibe mensagem de erro ao submeter o formulário com um email com formatação inválida', () => {
+    //----------------------------Exercício 02----------------------------//
+    it('Show error message when the form is submitted with an invalid email format', () => {
         cy.get('#firstName').type("Lucas ")
         cy.get('#lastName').type("Garcia Amorim")
-        cy.get('#email').type('Lucasgarciamorim@gmail.com2')
+        cy.get('#email').type('Lucasgarciamorim@gmail,com')
         cy.get('#phone').type('71999799831')
-        cy.get('#open-text-area').type('Testando o Cypress', {delay: 20})
+        cy.get('#open-text-area').type('Testing Cypress', {delay: 20})
+        cy.get('button[type="submit"]').click()
 
-        cy.get('.button').click()
+
         cy.get('.error').should('be.visible',)
 
     });
-    it('Se um valor numérico não for digitado no campo de telephone, ele continuará vazio', () => {
-        cy.get('#phone').type("ascvagf")
+
+    //----------------------------Exercício 03----------------------------//
+    it('If the phone field contains non-numeric characters, it remains empty', () => {
+        cy.get('#phone').type("Test")
+
+
         cy.get('#phone').should('have.value', '')
     });
-    it('Exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido antes do envio do formulário', () => {
+
+    //----------------------------Exercício 04----------------------------//
+    it('Display an error message when the phone number becomes mandatory but is not filled before submitting the form', () => {
         cy.get('#firstName').type("Lucas ")
         cy.get('#lastName').type("Garcia Amorim")
         cy.get('#email').type('Lucasgarciamorim@gmail.com')
         cy.get('#phone-checkbox').click()
-        cy.get('.button').click()
+        cy.get('button[type="submit"]').click()
+
+
         cy.get('.error').should('be.visible',)
 
     });
-    it('Exibe mensagem de erro ao submeter o formulário sem preencher os campos obrigatórios', () => {
-        cy.get('.button').click()
+
+    //----------------------------Exercício 05----------------------------//
+    it('Fill in and clear the fields: first name, last name, email, and phone', () => {
+        cy.get('#firstName').type("Lucas ")
+        cy.get('#lastName').type("Garcia Amorim")
+        cy.get('#email').type('Lucasgarciamorim@gmail.com')
+        cy.get('#phone').type('71999799831')
+
+        cy.get('#firstName').clear()
+        cy.get('#lastName').clear()
+        cy.get('#email').clear()
+        cy.get('#phone').clear()
+
+
+        cy.get('button[type="submit"]').click()
+
+
         cy.get('.error').should('be.visible')
 
     });
+
+    //----------------------------Exercício 06----------------------------//
+    it('Show error message if required fields are left empty upon form submission', () => {
+        cy.get('button[type="submit"]').click()
+
+        cy.get('.error').should('be.visible')
+
+    });
+
+    it('Form sent successfully with custom command', () => {
+        cy.fillMandatoryFieldsAndSubmit()
+
+
+
+    });
+
+
 })
